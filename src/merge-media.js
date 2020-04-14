170fdf397e09a7dd54a7291c7eb457d80be1c9ef
Maciej Wiczołek <maciej.wiczolek@outlook.com>
2020-04-14 15:55:17 +0200

    copy only unique files
    when we merge same docx few times then images are duplicated
    therfore output docx are getting big
    this change uses sha checksum to reduce image duplications

--- src/merge-media.js
+++ src/merge-media.js
@@ -1,11 +1,15 @@
 
 var XMLSerializer = require('xmldom').XMLSerializer;
 var DOMParser = require('xmldom').DOMParser;
+var crypto = require('crypto');
 
 
 var prepareMediaFiles = function(files, media) {
 
-       var count = 1;
+    var count = 1;
+
+    var fileTargetLookupTable = new Map();
+    var fileHashLookupTable = new Map();
 
     files.forEach(function(zip, index) {
         // var zip = new JSZip(file);
@@ -14,9 +18,22 @@ var prepareMediaFiles = function(files, media) {
         for (var mfile in medFiles) {
             if (/^word\/media/.test(mfile) && mfile.length > 11) {
                 // console.log(mfile);
+                var fileUncompressedSize = zip.file(mfile)._data.uncompressedSize;
+                if (!fileHashLookupTable.has(mfile)) {
+                    fileHashLookupTable.set(mfile, new Map());
+                }
+                if (!fileHashLookupTable.get(mfile).has(fileUncompressedSize)) {
+                    fileHashLookupTable.get(mfile).set(fileUncompressedSize, calculateFileHash(zip.file(mfile).asUint8Array()));
+                }
+
+                var fileHash = fileHashLookupTable.get(mfile).get(fileUncompressedSize);
+                if (!fileTargetLookupTable.has(fileHash)) {
+                    fileTargetLookupTable.set(fileHash, mfile.replace(/[0-9]/, '_' + count).replace('word/', ""));
+                }
+                
                 media[count] = {};
                 media[count].oldTarget = mfile;
-                media[count].newTarget = mfile.replace(/[0-9]/, '_' + count).replace('word/', "");
+                media[count].newTarget = fileTargetLookupTable.get(fileHash);
                 media[count].fileIndex = index;
                 updateMediaRelations(zip, count, media);
                 updateMediaContent(zip, count, media);
@@ -78,6 +95,13 @@ var copyMediaFiles = function(base, _media, _files) {
 
         base.file('word/' + _media[media].newTarget, content);
     }
+};
+
+var calculateFileHash = function (uint8array) {
+    var hash = crypto.createHash('sha1');
+    hash.setEncoding('hex');
+    hash.update(uint8array);
+    return hash.digest("hex");
 };
 
 module.exports = {
